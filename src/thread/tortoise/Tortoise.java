1e5215902ad4cedb01029464a8f4185678c453cf
This PC <devvvrats@gmail.com>
2020-10-29 17:12:52 +0530

    Completed Tortoise.java class.

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -8,14 +8,8 @@ public class Tortoise extends Thread {
 
     private static final int MILESTONES = 5;
 
-    private final Thread tortoise;
-
     public Tortoise() {
-        this.tortoise = new Thread(this, "tortoise");
-    }
-
-    public Thread getTortoise() {
-        return tortoise;
+        super("tortoise");
     }
 
     @Override
